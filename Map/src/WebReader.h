df881fb09ae1d7a5493932fed75291d8ba916029
Tolik <chyzh_tolik@ukr.net>
2021-03-12 22:09:03 +0200

    added test for webreader

--- Map/src/WebReader.h
+++ Map/src/WebReader.h
@@ -1,10 +1,11 @@
 #pragma once
-
+#define CURL_STATICLIB
+#include <curl/curl.h>
 
 class WebReader
 {
 public:
-	WebReader() {};
+	WebReader():resol(1), x(0), y(0){};
 	~WebReader() {};
 	void MakeFile(int resol,int x, int y);
 private:
--- UnitTests/src/tests.cpp
+++ UnitTests/src/tests.cpp
@@ -1,6 +1,10 @@
 #include <catch2/catch_all.hpp>
+#define CURL_STATICLIB
+#include <curl/curl.h>
 #include "URLReader.cpp"
-
+#include "WebReader.h"
+#include "WebReader.cpp"
+#include <iostream>
 
 TEST_CASE("Test URL params", "[URLReader]") 
 {
@@ -15,6 +19,14 @@ TEST_CASE("Test URL params", "[URLReader]")
     REQUIRE(url.MakeURL() == "www.google.com/4/5/6?access_token=554");
 }
 
+TEST_CASE("Test tile creation", "[WEBReader]")
+{
+    WebReader reader;
+    reader.MakeFile(1, 1, 1);
+    REQUIRE(std::filesystem::exists("Tiles/tile111.png") == true);
+    std::filesystem::remove("Tiles/tile111.png");
+}
+
 int main(int argc, char* argv[])
 {
     // global setup...
@@ -22,6 +34,6 @@ int main(int argc, char* argv[])
     int result = Catch::Session().run(argc, argv);
 
     // global clean-up...
-
+    std::cin.get();
     return result;
 }
